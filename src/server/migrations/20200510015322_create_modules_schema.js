29a7093ce309001fa0bace7d022f3899fd21bfeb
nuha90 <nuhanajah90@gmail.com>
2020-11-15 22:33:05 +0100

    create Topics table and insert topics to it

--- src/server/migrations/20200510015322_create_modules_schema.js
+++ src/server/migrations/20200510015322_create_modules_schema.js
@@ -1,24 +1,12 @@
 // TODO: This is a sample migration for demonstration. Remove before adding real ones.
 
 exports.up = function(knex) {
-  return knex.schema.createTable('modules', (table) => {
+  return knex.schema.createTable('Topics', (table) => {
     table.increments();
     table.string('title').notNullable();
-    table.datetime('startDate').notNullable();
-    table.datetime('endDate').notNullable();
-    table.string('classId').notNullable();
-    table
-      .datetime('createdAt')
-      .defaultTo(knex.fn.now())
-      .notNullable();
-    table
-      .datetime('updatedAt')
-      .defaultTo(knex.fn.now())
-      .notNullable();
-    table.datetime('deletedAt');
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('modules');
+  return knex.schema.dropTable('Topics');
 };
--- src/server/seeds/development/modules.js
+++ src/server/seeds/development/modules.js
@@ -2,24 +2,42 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('modules')
+  return knex('Topics')
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex('modules').insert([
+      return knex('Topics').insert([
         {
           id: 1,
-          title: 'Javascript 2',
-          startDate: '2020-05-10 00:00:00',
-          endDate: '2020-05-10 00:00:00',
-          classid: 'class01',
+          title: 'HTML/CSS',
         },
         {
           id: 2,
           title: 'Javascript 1',
-          startDate: '2020-05-07 00:00:00',
-          endDate: '2020-05-08 00:00:00',
-          classid: 'class01',
+        },
+        {
+          id: 3,
+          title: 'Javascript 2',
+        },
+        {
+          id: 4,
+          title: 'Javascript 3',
+        },
+        {
+          id: 5,
+          title: 'Databases',
+        },
+        {
+          id: 6,
+          title: 'Node.js',
+        },
+        {
+          id: 7,
+          title: 'React',
+        },
+        {
+          id: 8,
+          title: 'Final project',
         },
       ]);
     });
