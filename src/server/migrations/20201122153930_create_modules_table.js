5ffe8f9a7366c82373f1a5881b2ed24461d7c3f6
Yuka Fukuzawa <yfukuzawa318@gmail.com>
2020-11-23 13:00:54 +0100

    added migration modules table

---
+++ src/server/migrations/20201122153930_create_modules_table.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable
+  ('modules', (table) => {
+    table.increments();
+    table.varchar('module_name').notNullable();
+    table.datetime('created_at').notNullable();
+    table.datetime('updated_at').notNullable();
+    table.datetime('deleted_at').notNullable();
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('modules');
+};
