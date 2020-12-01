bb00da230870e82a5d94b84743e75ba2607d47fa
Daxious <daxiousm@gmail.com>
2020-12-01 18:54:02 +0100

    Add page404 component

---
+++ src/client/components/Page404/Page404.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AppHeader from '../AppHeader';
+import Footer from '../Footer';
+import Route404 from '../Route404';
+import './Page404.styles.css';
+
+const Page404 = () => {
+  return (
+    <div className="page404-container">
+      <AppHeader />
+      <Route404 />
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Page404;
---
+++ src/client/components/Page404/Page404.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Page404 from './Page404';
+
+export default { title: 'Page404' };
+
+export const Page404Menu = () => <Page404 />;
