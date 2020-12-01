784acf7c2dd8749c61a78883308ac326cba45303
Elia-Git <holeselv17@gmail.com>
2020-12-01 23:37:50 +0100

    I made a  typo error  correction

--- src/server/migrations/20201129124100_create_groups_schema.js
+++ src/server/migrations/20201129124100_create_groups_schema.js
@@ -1,7 +1,7 @@
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable('groups', (table) => {
     table.increments();
-    table.string('title').notNullable();
+    table.string('name').notNullable();
     table
       .integer('user_id')
       .unsigned()
@@ -9,18 +9,12 @@ exports.up = function(knex) {
       .references('id')
       .inTable('users')
       .onDelete('cascade');
-    table
-      .datetime('created_at')
-      .defaultTo(knex.fn.now())
-      .notNullable();
-    table
-      .datetime('updated_at')
-      .defaultTo(knex.fn.now())
-      .notNullable();
+    table.datetime('created_at').defaultTo(knex.fn.now()).notNullable();
+    table.datetime('updated_at').defaultTo(knex.fn.now()).notNullable();
     table.datetime('deleted_at');
   });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable('groups');
 };
