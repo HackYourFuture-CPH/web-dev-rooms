60e1aa5aa43e2ab824435e0409041d0c4c9c2043
Daxious <daxiousm@gmail.com>
2020-12-02 09:24:27 +0100

    header stories modified

--- src/client/components/Appheader/AppHeader.stories.js
+++ src/client/components/Appheader/AppHeader.stories.js
@@ -1,12 +1,6 @@
-import { withKnobs } from '@storybook/addon-knobs';
-import Vector from './Vector.svg'; // css and propreties come directly within the svg file
 import React from 'react';
-import { AppHeader } from './AppHeader.component'; // the component that you are working with
+import AppHeader from './AppHeader.component';
 
-export default {
-  // what you see @ bookstory list
-  title: 'AppHeader',
-  decorators: [withKnobs],
-};
+export default { title: 'AppHeader' };
 
-export const appHeader = () => <AppHeader path={Vector} />;
+export const AppHeaderMenu = () => <AppHeader />;
