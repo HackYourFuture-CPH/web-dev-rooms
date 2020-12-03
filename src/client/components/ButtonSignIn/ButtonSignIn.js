408da32c6cd780d50d1c07c0ad98d916454fffbc
AnilMula <anilreddymula@gmail.com>
2020-12-03 15:11:15 +0100

    Adjustments on the SignInButton Component as per requirement -trial2 to aviod merge conflict

--- src/client/components/ButtonSignIn/ButtonSignIn.js
+++ src/client/components/ButtonSignIn/ButtonSignIn.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './Button.css';
+import './ButtonSignIn.styles.css';
 import PropTypes from 'prop-types';
 
-const Button = (props) => {
-  const { onclick } = props;
-  const clicked = () => {
-    onclick();
-  };
+const ButtonSignIn = (props) => {
   return (
-    <button type="submit" className="button" onClick={clicked}>
+    <button
+      type="submit"
+      className="button-sign-with-slack"
+      onClick={props.onClick}
+    >
       <img
         alt="Sign in with Slack"
         src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
@@ -18,6 +18,7 @@ const Button = (props) => {
   );
 };
 // add propType for onclick function
-Button.propTypes = { onclick: PropTypes.func.isRequired };
+ButtonSignIn.propTypes = { onClick: PropTypes.func };
+ButtonSignIn.defaultProps = { onClick: null };
 
-export default Button;
+export default ButtonSignIn;
---
+++ src/client/components/ButtonSignIn/ButtonSignIn.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from './ButtonSignIn';
+
+export default {
+  title: 'ButtonSignIn',
+  component: Button,
+};
+
+export const ButtonSignIn = () => <Button />;
