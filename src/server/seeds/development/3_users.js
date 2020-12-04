e03f797e93f41441f1d5d48a9b47d2bd3e13f11b
Maher <maherhussain6@gmail.com>
2020-12-04 22:55:40 +0100

    delete the separate file for goup_iid reference

--- src/server/seeds/development/3_users.js
+++ src/server/seeds/development/3_users.js
@@ -6,7 +6,6 @@ exports.seed = function (knex) {
         {
           // mentor
           id: 1,
-          group_id: 1,
           name: 'user1',
           slack_id: 'u1',
           organization_id: 1,
@@ -15,7 +14,6 @@ exports.seed = function (knex) {
         {
           // student
           id: 2,
-          group_id: 2,
           name: 'user2',
           slack_id: 'u2',
           organization_id: 2,
@@ -24,7 +22,6 @@ exports.seed = function (knex) {
         {
           // student
           id: 3,
-          group_id: 3,
           name: 'user3',
           slack_id: 'u3',
           organization_id: 3,
@@ -33,7 +30,6 @@ exports.seed = function (knex) {
         {
           // stuff
           id: 4,
-          group_id: 4,
           name: 'user4',
           slack_id: 'u4',
           organization_id: 1,
@@ -42,7 +38,6 @@ exports.seed = function (knex) {
         {
           // mentor
           id: 5,
-          group_id: 5,
           name: 'user5',
           slack_id: 'u5',
           organization_id: 2,
@@ -50,7 +45,6 @@ exports.seed = function (knex) {
         },
         {
           id: 6,
-          group_id: 6,
           name: 'user6',
           slack_id: 'u6',
           organization_id: 1,
@@ -58,7 +52,6 @@ exports.seed = function (knex) {
         },
         {
           id: 7,
-          group_id: 10,
           name: 'user7',
           slack_id: 'u7',
           organization_id: 2,
@@ -66,7 +59,6 @@ exports.seed = function (knex) {
         },
         {
           id: 8,
-          group_id: 8,
           name: 'user8',
           slack_id: 'u8',
           organization_id: 3,
@@ -74,7 +66,6 @@ exports.seed = function (knex) {
         },
         {
           id: 9,
-          group_id: 9,
           name: 'user9',
           slack_id: 'u9',
           organization_id: 5,
@@ -82,7 +73,6 @@ exports.seed = function (knex) {
         },
         {
           id: 10,
-          group_id: 9,
           name: 'user10',
           slack_id: 'u10',
           organization_id: 2,
