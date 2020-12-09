a3639b8595758bac989795f445a964de4997f62b
nuha90 <nuhanajah90@gmail.com>
2020-12-09 21:55:11 +0100

    feedback implemented

--- src/client/containers/LoginPage/LoginPage.component.js
+++ src/client/containers/LoginPage/LoginPage.component.js
@@ -4,7 +4,7 @@ import { HyfLogo } from '../../components/HyfBigLogo/HyfBigLogo.stories';
 
 export default function LoginPage() {
   const authenticateUser = () => {
-    const clientId = process.env.SLACK__CLIENT_ID;
+    const clientId = process.env.SLACK_CLIENT_ID;
     const oauthUrl = `https://slack.com/oauth/v2/authorize?client_id=${clientId}&user_scope=identity.basic`;
     window.location = oauthUrl;
   };
