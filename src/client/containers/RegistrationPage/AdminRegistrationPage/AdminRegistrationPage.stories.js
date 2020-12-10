36f281739196ff1108d2fedc3f3efaebf3f929f8
Deepti <deepti.sharma@gmail.com>
2020-12-10 21:41:40 +0100

    admin reg files deleted from this branch

--- src/client/containers/RegistrationPage/AdminRegistrationPage/AdminRegistrationPage.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { AdminRegistrationPage } from './AdminRegistrationPage.component';
-
-export default {
-  title: 'AdminRegistrationPage',
-};
-
-export const AdminRegistration = () => <AdminRegistrationPage />;
