8e7bbd9548575ef84277e6b2b0c9d861c3eddec1
prabin <1620prabin@gmail.com>
2020-12-11 14:39:55 +0100

    feedback implemented

--- src/client/components/ModalWithFeedback/ModalWithFeedback.js
+++ src/client/components/ModalWithFeedback/ModalWithFeedback.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '../Button/Button';
+import PropTypes, { func } from 'prop-types';
 import './ModalWithFeedback.css';
-import { Card } from '../Card/Card';
+import Input from '../Input/Input';
 import { EventCardText } from '../EventCardText/EventCardText';
 import Modal from '../Modal/Modal';
 
-function ModalWithFeedback() {
+function ModalWithFeedback(props) {
+  const [name, setName] = useState('');
   return (
     <div className="feedback-modal">
       <Modal title="Feedback Form">
@@ -13,22 +15,45 @@ function ModalWithFeedback() {
           <span className="dot" />
           <div className="eventcardtext">
             <EventCardText
-              title="Elastic - Week 46"
-              datetime="Wednesday | 11 Nov 20|10:00 CEST"
-              mentor="'Mentor Name'"
-              link="https://slack.com/meeting...."
+              title={props.title}
+              datetime={props.datetime}
+              mentor={props.mentor}
+              link={props.link}
             />
           </div>
         </div>
         <div className="feedback-textfield">
-          <Card text="You just attended the study group with student1, student2, student3, student4......." />
+          <Input
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            placeholder="
+You just attended the study group with student1,
+ student2, student3, student4......."
+          />
         </div>
         <div className="feedback-submit-btn">
-          <Button>Submit</Button>
+          <Button onClick={props.onSubmit}>Submit</Button>
         </div>
       </Modal>
     </div>
   );
 }
+ModalWithFeedback.propTypes = {
+  title: PropTypes.string,
+  datetime: PropTypes.string,
+  mentor: PropTypes.string,
+  link: PropTypes.string,
+  onSubmit: func,
+};
+
+ModalWithFeedback.defaultProps = {
+  title: '',
+  datetime: '',
+  mentor: '',
+  link: '',
+  onSubmit: null,
+};
 
 export default ModalWithFeedback;
--- src/client/components/ModalWithFeedback/ModalWithFeedback.stories.js
+++ src/client/components/ModalWithFeedback/ModalWithFeedback.stories.js
@@ -4,4 +4,11 @@ import ModalWithFeedback from './ModalWithFeedback';
 
 export default { title: 'Modal with feedback' };
 
-export const modalwithfeedback = () => <ModalWithFeedback />;
+export const modalwithfeedback = () => (
+  <ModalWithFeedback
+    title="Elastic - Week 46"
+    datetime="Wednesday | 11 Nov 20|10:00 CEST"
+    mentor="'Mentor Name'"
+    link="https://slack.com/meeting...."
+  />
+);
