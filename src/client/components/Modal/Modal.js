b1eb586724ecf0115c207966f726c978a744cafd
Deepti <deepti.sharma@gmail.com>
2020-12-11 20:12:00 +0100

    create-event modal created

--- src/client/components/Modal/Modal.js
+++ src/client/components/Modal/Modal.js
@@ -5,15 +5,20 @@ import PropTypes from 'prop-types';
 export default function Modal(props) {
   return (
     <div className="modal-wrapper">
-      <div className="modal-header">
-        <span className="close-modal-btn">x</span>
-      </div>
-      <h4 className="modal-title">{props.title} </h4>
-      {props.children}
+      <button
+        type="button"
+        className="modal-close-button"
+        onClick={props.onClose}
+      >
+        ✕
+      </button>
+      {props.title && <h4 className="modal-title">{props.title} </h4>}
+      <div className="modal-content">{props.children}</div>
     </div>
   );
 }
 Modal.propTypes = {
   title: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
 };
---
+++ src/client/containers/ModalCreateEvents/ModalCreateEvents.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import ModalCreateEvents from './ModalCreateEvents';
+
+export default { title: 'Modal to create events' };
+
+export const modalCreateEvents = () => <ModalCreateEvents />;
