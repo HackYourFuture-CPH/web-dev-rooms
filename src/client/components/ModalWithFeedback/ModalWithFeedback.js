fc335fd1fe819b3a4b37840c5ce6b0e416e3c433
prabin <1620prabin@gmail.com>
2020-12-12 11:24:02 +0100

    added onclick handler

--- src/client/components/ModalWithFeedback/ModalWithFeedback.js
+++ src/client/components/ModalWithFeedback/ModalWithFeedback.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 import { Button } from '../Button/Button';
-import PropTypes, { func } from 'prop-types';
+import PropTypes from 'prop-types';
 import './ModalWithFeedback.css';
 import Input from '../Input/Input';
 import { EventCardText } from '../EventCardText/EventCardText';
 import Modal from '../Modal/Modal';
 
 function ModalWithFeedback(props) {
-  const [name, setName] = useState('');
+  const [text, textName] = useState('');
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="feedback-modal">
       <Modal title="Feedback Form">
@@ -24,36 +29,34 @@ function ModalWithFeedback(props) {
         </div>
         <div className="feedback-textfield">
           <Input
-            value={name}
+            value={text}
             onChange={(e) => {
-              setName(e.target.value);
+              textName(e.target.value);
             }}
-            placeholder="
-You just attended the study group with student1,
- student2, student3, student4......."
+            placeholder={props.placeholder}
           />
         </div>
         <div className="feedback-submit-btn">
-          <Button onClick={props.onSubmit}>Submit</Button>
+          <Button onClick={handelSubmit}>Submit</Button>
         </div>
       </Modal>
     </div>
   );
 }
 ModalWithFeedback.propTypes = {
+  placeholder: PropTypes.string,
   title: PropTypes.string,
   datetime: PropTypes.string,
   mentor: PropTypes.string,
   link: PropTypes.string,
-  onSubmit: func,
 };
 
 ModalWithFeedback.defaultProps = {
+  placeholder: '',
   title: '',
   datetime: '',
   mentor: '',
   link: '',
-  onSubmit: null,
 };
 
 export default ModalWithFeedback;
--- src/client/components/ModalWithFeedback/ModalWithFeedback.stories.js
+++ src/client/components/ModalWithFeedback/ModalWithFeedback.stories.js
@@ -6,6 +6,7 @@ export default { title: 'Modal with feedback' };
 
 export const modalwithfeedback = () => (
   <ModalWithFeedback
+    placeholder=" You just attended the study group with student1, student2, student3, student4......."
     title="Elastic - Week 46"
     datetime="Wednesday | 11 Nov 20|10:00 CEST"
     mentor="'Mentor Name'"
