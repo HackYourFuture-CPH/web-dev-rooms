37f357192630a5e7777e2b131d26f1a1f3ea3cdc
AnilMula <anilreddymula@gmail.com>
2020-12-13 16:13:58 +0100

    added onClick on button

--- src/client/components/ModalViewCancelEvent/ModalViewCancelEvent.js
+++ src/client/components/ModalViewCancelEvent/ModalViewCancelEvent.js
@@ -3,11 +3,16 @@ import Modal from '../Modal/Modal';
 import { Button } from '../Button/Button';
 
 export const ModalCancelEvent = () => {
+  const handleOnClick = () => {
+    return null;
+  };
   return (
     <div>
       <Modal title="Cancel Event">
         <p>Are you sure you want to cancel</p>
-        <Button className="modal-cancel-event-button">Cancel</Button>
+        <Button className="modal-cancel-event-button" onClick={handleOnClick}>
+          Cancel
+        </Button>
       </Modal>
     </div>
   );
