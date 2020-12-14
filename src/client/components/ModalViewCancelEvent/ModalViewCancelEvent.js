1b6013b426321427bbdaf16f10b0f5e7a3261b32
AnilMula <anilreddymula@gmail.com>
2020-12-14 20:41:01 +0100

    passed onClick as props

--- src/client/components/ModalViewCancelEvent/ModalViewCancelEvent.js
+++ src/client/components/ModalViewCancelEvent/ModalViewCancelEvent.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import Modal from '../Modal/Modal';
 import { Button } from '../Button/Button';
+import PropTypes from 'prop-types';
 
-export const ModalCancelEvent = () => {
-  const handleOnClick = () => {
-    return null;
-  };
+export const ModalCancelEvent = (props) => {
   return (
     <div>
       <Modal title="Cancel Event">
         <p>Are you sure you want to cancel</p>
-        <Button className="modal-cancel-event-button" onClick={handleOnClick}>
+        <Button className="modal-cancel-event-button" onClick={props.onClick}>
           Cancel
         </Button>
       </Modal>
     </div>
   );
+};
+Button.propTypes = {
+  onClick: PropTypes.func,
+};
+Button.defaultProps = {
+  onClick: null,
 };
