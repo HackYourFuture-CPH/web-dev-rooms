98b023b654bf41c0bdcebc82cad05ba5dc315040
Maher <maherhussain6@gmail.com>
2020-12-15 00:42:39 +0100

    feedback is implemented

--- src/client/containers/Admin-Event-details/Admin-Event-modal-view.js
+++ src/client/containers/Admin-Event-details/Admin-Event-modal-view.js
@@ -2,11 +2,31 @@ import React from 'react';
 import { EventCardText } from '../../components/EventCardText/EventCardText';
 import Modal from '../../components/Modal/Modal';
 import { StudyGroupLogo } from '../../components/StudyGroupLogo/StudyGroupLogo';
-
+import PropTypes from 'prop-types';
 import Elasticlogo from './Elastic.logo.svg';
 import './Admin-event-modal.style.css';
 
-export default function EventModalView() {
+export default function EventModalView(props) {
+  const students = [
+    {
+      studentName: 'DemoStudent1',
+      class: 'class 15',
+      topic: 'Topic: JavaScript week3 homework,classes,Function',
+      code: 'maherweb.dk',
+    },
+    {
+      studentName: 'DemoStudent2',
+      calss: 'class 15',
+      topic: 'Topic: JavaScript week3 homework,classes,Function',
+      code: 'maherweb.dk',
+    },
+    {
+      studentName: 'DemoStudent3',
+      calss: 'class 15',
+      topic: 'Topic: JavaScript week3 homework,classes,Function',
+      code: 'maherweb.dk',
+    },
+  ];
   return (
     <div className="event-modal-view">
       <Modal>
@@ -16,67 +36,40 @@ export default function EventModalView() {
           </div>
           <div className="card-text">
             <EventCardText
-              title="Elastic - Week 46"
-              datetime="Wednesday | 11 Nov 20|10:00 CEST"
-              mentor="Maher"
-              link="www.hyf.org"
+              title={props.eventTitle}
+              datetime={props.eventDate}
+              mentor={props.mentor}
+              link={props.eventLink}
             />
           </div>
         </div>
         <div className="student-details-block">
-          <div className="student-info">
-            <p>
-              DemoStudent1|class15 <span className="close-btn">x</span>
-            </p>
+          {students.map((elem) => {
+            return (
+              <div className="student-info">
+                <p>
+                  {elem.studentName}|{elem.class}
+                  <span className="close-btn">x</span>
+                </p>
 
-            <p>Topic: JavaScript week3 homework,classes,Function</p>
-            <p>
-              code :
-              <a
-                href="maherweb.dk"
-                className="student-code-link"
-                target="_blank"
-              >
-                maherweb.dk
-              </a>
-            </p>
-          </div>
-          <div className="student-info">
-            <p>
-              DemoStudent2|class15 <span className="close-btn">x</span>
-            </p>
-
-            <p>Topic: JavaScript week3 homework,classes,Function</p>
-            <p>
-              code :
-              <a
-                href="maherweb.dk"
-                className="student-code-link"
-                target="_blank"
-              >
-                maherweb.dk
-              </a>
-            </p>
-          </div>
-          <div className="student-info">
-            <p>
-              DemoStudent3|class15 <span className="close-btn">x</span>
-            </p>
-
-            <p>Topic: JavaScript week3 homework,classes,Function</p>
-            <p>
-              code :
-              <a
-                href="maherweb.dk"
-                className="student-code-link"
-                target="_blank"
-              >
-                maherweb.dk
-              </a>
-            </p>
-          </div>
+                <p>Topic: {elem.topic}</p>
+                <p>
+                  code :
+                  <a href={elem.code} className="student-code-link">
+                    {elem.code}
+                  </a>
+                </p>
+              </div>
+            );
+          })}
         </div>
       </Modal>
     </div>
   );
 }
+EventModalView.propTypes = {
+  eventTitle: PropTypes.string.isRequired,
+  eventDate: PropTypes.string.isRequired,
+  mentor: PropTypes.string.isRequired,
+  eventLink: PropTypes.string.isRequired,
+};
--- src/client/containers/Admin-Event-details/Admin-Modal-View.stories.js
+++ src/client/containers/Admin-Event-details/Admin-Modal-View.stories.js
@@ -3,4 +3,11 @@ import AdminModalView from './Admin-Event-modal-view';
 
 export default { title: 'AdminModalView' };
 
-export const EventViewModal = () => <AdminModalView />;
+export const EventViewModal = () => (
+  <AdminModalView
+    eventTitle="Elastic - Week 46"
+    eventDate="Wednesday | 11 Nov 20|10:00 CEST"
+    mentor="madsssssssssssssssssssssssssssssssssssssssssss"
+    eventLink="www.hyf.org"
+  />
+);
