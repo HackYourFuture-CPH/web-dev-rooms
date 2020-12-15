9e8f59f9e72b31271cf19c6d4d31eb9822bc5d7f
Maher <maherhussain6@gmail.com>
2020-12-15 21:54:08 +0100

    adding view modal for event

---
+++ src/client/containers/Admin-Event-details/Admin-Event-modal-view.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { EventCardText } from '../../components/EventCardText/EventCardText';
+import Modal from '../../components/Modal/Modal';
+import { StudyGroupLogo } from '../../components/StudyGroupLogo/StudyGroupLogo';
+import PropTypes from 'prop-types';
+import Elasticlogo from './Elastic.logo.svg';
+import './Admin-event-modal.style.css';
+
+export default function EventModalView(props) {
+  const students = [
+    {
+      studentName: 'DemoStudent1',
+      class: 'class 15',
+      topic: 'Topic: JavaScript week3 homework,classes,Function',
+      code: 'maherweb.dk',
+    },
+    {
+      studentName: 'DemoStudent2',
+      class: 'class 15',
+      topic: 'Topic: JavaScript week3 homework,classes,Function',
+      code: 'maherweb.dk',
+    },
+    {
+      studentName: 'DemoStudent3',
+      class: 'class 15',
+      topic: 'Topic: JavaScript week3 homework,classes,Function',
+      code: 'maherweb.dk',
+    },
+  ];
+  return (
+    <div className="event-modal-view">
+      <Modal>
+        <div className="study-group-info">
+          <div className="study-group-logo">
+            <StudyGroupLogo path={Elasticlogo} />
+          </div>
+          <div className="card-text">
+            <EventCardText
+              title={props.eventTitle}
+              datetime={props.eventDate}
+              mentor={props.mentor}
+              link={props.eventLink}
+            />
+          </div>
+        </div>
+        <div className="student-details-block">
+          {students.map((elem) => {
+            return (
+              <div className="student-info">
+                <p>
+                  {elem.studentName} | {elem.class}
+                  <span className="close-btn">x</span>
+                </p>
+
+                <p>Topic: {elem.topic}</p>
+                <p>
+                  code :
+                  <a href={elem.code} className="student-code-link">
+                    {elem.code}
+                  </a>
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </Modal>
+    </div>
+  );
+}
+EventModalView.propTypes = {
+  eventTitle: PropTypes.string,
+  eventDate: PropTypes.string,
+  mentor: PropTypes.string,
+  eventLink: PropTypes.string,
+};
+EventModalView.defaultProps = {
+  eventTitle: '',
+  eventDate: '',
+  mentor: '',
+  eventLink: '',
+};
---
+++ src/client/containers/Admin-Event-details/Admin-Modal-View.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AdminModalView from './Admin-Event-modal-view';
+
+export default { title: 'AdminModalView' };
+
+export const EventViewModal = () => (
+  <AdminModalView
+    eventTitle="Elastic - Week 46"
+    eventDate="Wednesday | 11 Nov 20|10:00 CEST"
+    mentor="madsssssssssssssssssssssssssssssssssssssssssss"
+    eventLink="maherweb.dk"
+  />
+);
