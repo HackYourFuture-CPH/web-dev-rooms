d348e5156db0684823fa92d800c86ebc29adf4e7
Toni Petrina <tonipetrina@hotmail.com>
2020-12-20 13:41:32 +0100

    Remove duplicate Router

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import AppContainer from './AppContainer';
 
 import './index.css';
 
-ReactDOM.render(
-  <Router>
-    <AppContainer />
-  </Router>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<AppContainer />, document.getElementById('root'));
