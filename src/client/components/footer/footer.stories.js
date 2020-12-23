8250d772d37f1969b4567ab5bb914ff1fc0be2bc
Yaser-Aldammad <yaser_dammad@hotmail.com>
2020-12-23 15:53:52 +0100

    Revert "footer display in storybook after adding linkage properity"
    This reverts commit 0ea10285c083872dbfc8f5d080f99071a72a1a3a.

--- src/client/components/footer/footer.stories.js
+++ src/client/components/footer/footer.stories.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import StoryRouter from 'storybook-react-router';
 import Footer from './footer';
 
-const stories = storiesOf('Footer', module);
-stories.addDecorator(StoryRouter());
-stories.add('Example', () => <Footer />);
+export default { title: 'footer' };
+
+export const FooterMenu = () => <Footer />;
