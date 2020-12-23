495d152e0915c8937fe24723787bf6bb4ec516a9
Yaser-Aldammad <yaser_dammad@hotmail.com>
2020-12-23 13:10:11 +0100

    footer link-Router

--- src/client/components/footer/footer.js
+++ src/client/components/footer/footer.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../../context/userContext';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 import './footer.styles.css';
 import home from './home.png';
@@ -17,20 +17,22 @@ const Footer = () => {
     }
   };
   return (
-    <footer>
-      <div className="main-content">
-        <div className="box">
-          <img src={home} alt="home icon" />
-          <h4>HOME</h4>
+    <Router>
+      <footer>
+        <div className="main-content">
+          <div className="box">
+            <img src={home} alt="home icon" />
+            <h4>HOME</h4>
+          </div>
+          <div className=" box">
+            <Link to={getProfileLink()}>
+              <img src={profile} alt="profile icon" />
+              <h4>PROFILE</h4>
+            </Link>
+          </div>
         </div>
-        <div className=" box">
-          <Link to={getProfileLink()}>
-            <img src={profile} alt="profile icon" />
-            <h4>PROFILE</h4>
-          </Link>
-        </div>
-      </div>
-    </footer>
+      </footer>
+    </Router>
   );
 };
 
