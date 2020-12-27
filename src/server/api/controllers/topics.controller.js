a39dbd3fdddd3928ce4b53f421907477424f6642
Pallabi Ghosh Das <pallabighosh3100@gmail.com>
2020-12-27 22:45:16 +0100

    API: create, update, delete topics

--- src/server/api/controllers/topics.controller.js
+++ src/server/api/controllers/topics.controller.js
@@ -1,5 +1,6 @@
 const knex = require('../../config/db');
 const Error = require('../lib/utils/http-error');
+const moment = require('moment-timezone');
 
 const getTopics = async () => {
   try {
@@ -26,7 +27,41 @@ const getTopicById = async (id) => {
   }
 };
 
+const editTopic = async (topicId, body) => {
+  return knex('topics')
+    .where({ id: topicId })
+    .update({
+      topic_name: body.topicName,
+      created_at: moment(body.created_at).format(),
+      updated_at: moment(body.updated_at).format(),
+    });
+};
+
+const deleteTopic = async (topicId) => {
+  return knex('topics').where({ id: topicId }).del();
+};
+
+const createTopic = async (body) => {
+  await knex('topics').insert({
+    topic_name: body.topicName,
+    week_number: body.weekNumber,
+    readme_url: body.readmeUrl,
+    lesson_url: body.lessonUrl,
+    homework_url: body.homeworkUrl,
+    module_id: body.moduleId,
+    created_at: moment(body.created_at).format(),
+    updated_at: moment(body.updated_at).format(),
+  });
+
+  return {
+    successful: true,
+  };
+};
+
 module.exports = {
   getTopics,
   getTopicById,
+  editTopic,
+  deleteTopic,
+  createTopic,
 };
--- src/server/api/routes/topics.router.js
+++ src/server/api/routes/topics.router.js
@@ -54,4 +54,134 @@ router.get('/:id', (req, res, next) => {
     .catch(next);
 });
 
+/**
+ * @swagger
+ * /topics:
+ *  post:
+ *    summary: Create a topic
+ *    description:
+ *      Will create a topic.
+ *    produces: application/json
+ *    parameters:
+ *      - in: body
+ *        name: topic
+ *        description: The topic to create.
+ *        schema:
+ *          type: object
+ *          required:
+ *            - topicName
+ *            - weekNumber
+ *            - readmeUrl
+ *            - lessonUrl
+ *            - homeworkUrl
+ *            - moduleId
+ *            - created_at
+ *            - updated_at
+ *          properties:
+ *            topicName:
+ *              type: string
+ *            weekNumber:
+ *              type: integer
+ *            readmeUrl:
+ *              type: varchar
+ *            lessonUrl:
+ *              type: varchar
+ *            homeworkUrl:
+ *              type: varchar
+ *            moduleId:
+ *              type: integer
+ *            created_at:
+ *              type: string
+ *              format: date-time
+ *            updated_at:
+ *              type: string
+ *              format: date-time
+ *    responses:
+ *      200:
+ *        description: Successful request
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.post('/', (req, res) => {
+  topicsController
+    .createTopic(req.body)
+    .then((result) => res.json(result))
+    .catch((error) => {
+      console.log(error);
+
+      res.status(400).send('Bad request').end();
+    });
+});
+
+/**
+ * @swagger
+ * /topics/{ID}:
+ *  patch:
+ *    summary: Create a topic
+ *    description:
+ *      Will create a topic.
+ *    produces: application/json
+ *    parameters:
+ *      - in: path
+ *        name: ID
+ *        description: ID of the topic to patch.
+ *      - in: body
+ *        name: topic
+ *        description: The topic to create.
+ *        schema:
+ *          type: object
+ *          properties:
+ *            topicName:
+ *              type: string
+ *            created_at:
+ *              type: string
+ *              format: date-time
+ *            updated_at:
+ *              type: string
+ *              format: date-time
+ *    responses:
+ *      200:
+ *        description: Topic was patched
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.patch('/:id', (req, res, next) => {
+  topicsController
+    .editTopic(req.params.id, req.body)
+    .then((result) => res.json(result))
+    .catch(next);
+});
+
+/**
+ * @swagger
+ * /topics/{ID}:
+ *  delete:
+ *    summary: Delete a topic
+ *    description:
+ *      Will delete a topic with a given ID.
+ *    produces: application/json
+ *    parameters:
+ *      - in: path
+ *        name: ID
+ *        description: ID of the topic to delete.
+ *    responses:
+ *      200:
+ *        description: Topic deleted
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.delete('/:id', (req, res) => {
+  topicsController
+    .deleteTopic(req.params.id, req)
+    .then((result) => {
+      // If result is equal to 0, then that means the topic id does not exist
+      if (result === 0) {
+        res.status(404).send('The topic ID you provided does not exist.');
+      } else {
+        res.json({ success: true });
+      }
+    })
+    .catch((error) => console.log(error));
+});
+
 module.exports = router;
