9d5130deb3dfaefedeaef15d4a5059e9b5ae90eb
Yuka Fukuzawa <yfukuzawa318@gmail.com>
2020-12-28 10:25:30 +0100

    add StaticRouter in footerstory and removed Router in footer

--- src/client/components/footer/footer.js
+++ src/client/components/footer/footer.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../../context/userContext';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './footer.styles.css';
 import home from './home.png';
@@ -19,22 +19,18 @@ const Footer = () => {
   return (
     <footer>
       <div className="main-content">
-        <Router>
-          <div className="box">
-            <Link to="/home">
-              <img src={home} alt="home icon" />
-              <h4>HOME</h4>
-            </Link>
-          </div>
-        </Router>
-        <Router>
-          <div className=" box">
-            <Link to={getProfileLink()}>
-              <img src={profile} alt="profile icon" />
-              <h4>PROFILE</h4>
-            </Link>
-          </div>
-        </Router>
+        <div className="box">
+          <Link to="/home">
+            <img src={home} alt="home icon" />
+            <h4>HOME</h4>
+          </Link>
+        </div>
+        <div className=" box">
+          <Link to={getProfileLink()}>
+            <img src={profile} alt="profile icon" />
+            <h4>PROFILE</h4>
+          </Link>
+        </div>
       </div>
     </footer>
   );
