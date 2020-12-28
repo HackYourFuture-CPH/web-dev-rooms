c5090c0ede5343630334d29993f42314c82840b2
prabin <1620prabin@gmail.com>
2020-12-28 22:06:38 +0100

    endpoints for creating, updating, and deleting skills

--- src/server/api/controllers/skills.controller.js
+++ src/server/api/controllers/skills.controller.js
@@ -1,5 +1,6 @@
 const knex = require('../../config/db');
 const Error = require('../lib/utils/http-error');
+const moment = require('moment-timezone');
 
 const getSkills = async () => {
   try {
@@ -12,7 +13,7 @@ const getSkills = async () => {
 const getSkillById = async (id) => {
   try {
     const skills = await knex('skills')
-      .select('skills.id as id', 'name')
+      .select('skills.id as id', 'skills.name as skillName')
       .where({ id });
     if (skills.length === 0) {
       throw new Error(`Incorrect entry with the id of ${id}`, 404);
@@ -23,7 +24,35 @@ const getSkillById = async (id) => {
   }
 };
 
+const createSkill = async (body) => {
+  const today = moment().format('YYYY-MM-DD HH:mm:ss');
+  await knex('skills').insert({
+    name: body.skillName,
+    created_at: today,
+    updated_at: today,
+  });
+
+  return {
+    successful: true,
+  };
+};
+const editSkill = async (skillId, body) => {
+  const today = moment().format('YYYY-MM-DD HH:mm:ss');
+  return knex('skills').where({ id: skillId }).update({
+    name: body.skillName,
+    updated_at: today,
+  });
+};
+
+const deleteSkill = async (skillId) => {
+  const today = moment().format('YYYY-MM-DD HH:mm:ss');
+  return knex('skills').where({ id: skillId }).update({ deleted_at: today });
+};
+
 module.exports = {
   getSkills,
   getSkillById,
+  createSkill,
+  editSkill,
+  deleteSkill,
 };
