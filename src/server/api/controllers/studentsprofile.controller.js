dc5232d4a8f26bef8e5bd143f9009496f869b5b9
Yuka Fukuzawa <yfukuzawa318@gmail.com>
2021-01-04 17:39:45 +0100

    deleted migration and seed for student profile and changed studentsprofile controller

--- src/server/api/controllers/studentsprofile.controller.js
+++ src/server/api/controllers/studentsprofile.controller.js
@@ -3,12 +3,11 @@ const Error = require('../lib/utils/http-error');
 
 const getStudentsProfile = async () => {
   try {
-    return await knex('students_profile').select(
-      'name',
-      'group_id',
-      'group_name',
-      'time_zone',
-    );
+    const studentsProfile = await knex('users')
+      .select('name', 'group_id', 'groups.name as group_name', 'timezone')
+      .leftJoin('groups', 'group_id', 'groups.id')
+      .leftJoin('groups', 'group_name', 'groups.name');
+    return studentsProfile;
   } catch (error) {
     return error.message;
   }
@@ -16,13 +15,13 @@ const getStudentsProfile = async () => {
 
 const getStudentProfileById = async (id) => {
   try {
-    const event = await knex('students_profile')
+    const studentProfile = await knex('users')
       .select('group_id as id', 'name')
       .where({ id });
-    if (event.length === 0) {
+    if (studentProfile.length === 0) {
       throw new Error(`incorrect entry with the id of ${id}`, 404);
     }
-    return event;
+    return studentProfile;
   } catch (error) {
     return error.message;
   }
