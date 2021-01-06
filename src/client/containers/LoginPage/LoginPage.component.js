2aa07e18e5b65bb3d8495b31b856028455138af1
Toni Petrina <tonipetrina@hotmail.com>
2021-01-06 16:52:20 +0100

    Fix Slack login

--- src/client/containers/LoginPage/LoginPage.component.js
+++ src/client/containers/LoginPage/LoginPage.component.js
@@ -7,7 +7,7 @@ import Button from '../../components/SignInButton/Button';
 
 export default function LoginPage() {
   const authenticateUser = () => {
-    const clientId = process.env.SLACK_CLIENT_ID;
+    const clientId = process.env.REACT_APP_SLACK_CLIENT_ID;
     const oauthUrl = `https://slack.com/oauth/v2/authorize?client_id=${clientId}&user_scope=identity.basic`;
     window.location = oauthUrl;
   };
