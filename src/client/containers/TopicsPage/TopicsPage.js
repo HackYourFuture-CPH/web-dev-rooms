d12d5fe1ca4d6a5e12049b0e03a026ee7d714c23
Pallabi Ghosh Das <pallabighosh3100@gmail.com>
2021-01-11 12:26:50 +0100

    Page: Create a page that will list all topics #261

---
+++ src/client/containers/TopicsPage/TopicsPage.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { Layout } from '../../components/Layout';
+import Footer from '../../components/footer/footer';
+import { AppHeader } from '../../components/Appheader/AppHeader.component';
+import { AdminAvatar } from '../../components/Avatar';
+import Heading from '../../components/Heading/Heading';
+import Label from '../../components/Label/Tags/Label';
+import { useAuthenticatedFetch } from '../../hooks/useAuthenticatedFetch';
+
+export default function TopicsPage() {
+  const [topics, setTopics] = useState([]);
+  const { fetch } = useAuthenticatedFetch();
+
+  useEffect(() => {
+    fetch('/api/topics').then((data) => {
+      setTopics(data);
+      console.log(data);
+    });
+  }, [fetch]);
+
+  return (
+    <Layout>
+      <AppHeader />
+      <div className="admin-registration-avatar">
+        <AdminAvatar />
+      </div>
+      <Heading>Welcome Admin</Heading>
+      <div className="topic-lable">
+        {topics.map((topic) => (
+          <Label text={topic.topicName} />
+        ))}
+      </div>
+      <Footer id="footer" />
+    </Layout>
+  );
+}
