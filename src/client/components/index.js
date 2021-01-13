b5431ee2831d30fcd99401a6f15dbd34d0cdc84e
Toni Petrina <tonipetrina@hotmail.com>
2021-01-13 19:32:24 +0100

    Footer and responsive design

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -1,7 +1,10 @@
+import Footer from './footer/footer';
 import Heading from './Heading/Heading';
 import HelpText from './HelpText/HelpText';
-import Loader from './Loader';
 import { Layout } from './Layout';
+import Loader from './Loader';
 
+export * from './Appheader/AppHeader.component';
+export * from './Appheader/AppLogo';
 export * from './Button/Button';
-export { Heading, HelpText, Layout, Loader };
+export { Footer, Heading, HelpText, Layout, Loader };
